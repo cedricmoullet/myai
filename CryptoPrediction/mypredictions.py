042ab63c7dd5d4be25dbde8b133ba9cbcb2642d5
Cédric Moullet <cedricmoullet@MBP-de-Cedric-2.fritz.box>
2021-01-24 05:42:12 +0100

    Add a new predictor with data collected from my portfolio

---
+++ CryptoPrediction/mypredictions.py
@@ -0,0 +1,30 @@
+import requests
+import io
+import pandas as pd
+import matplotlib.dates as mdates
+import seaborn as sns
+from datetime import datetime
+
+path = 'https://drive.google.com/uc?export=download&id=1mQr7hY6yO88nv5SmLbYBRk14cJJ_FQnP'
+
+s = requests.get(path).content
+dateparse = lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+df = pd.read_csv(io.StringIO(s.decode('utf-8')),
+                 header='infer',
+                 delimiter=',',
+                 parse_dates=['Date'],
+                 date_parser=dateparse)
+
+print(df.info())
+
+ax = df.plot(x="Date", y="BTC")
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+ax.set_xlabel("Date")
+
+# Create a pairplot
+sns.pairplot(df,
+             height=2.5,
+             x_vars=['BTC', 'ETH', 'LTC'],
+             y_vars=['BTC', 'ETH', 'LTC']
+             )
+
